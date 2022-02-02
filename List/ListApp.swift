62e9cac94bff9af059b21ffd4d6f0d6d5150138b
Etienne Vautherin <etienne@vautherin.com>
2022-02-02 09:57:49 +0100

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Etienne Vautherin on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
